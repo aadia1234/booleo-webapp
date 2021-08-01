dc372a5f92e8257cba1129a7b1ef52f3e15af1bf
gam3boy1 <aadianand1234@gmail.com>
2021-08-01 18:20:02 +0800

    test nemobot

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const cool = require("cool-ascii-faces");
 const express = require('express')
 const app = express();
+const axios = require("axios").default;
 // const jsdom = require('jsdom');
 // const $ = require('jquery')(new jsdom.JSDOM().window);
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.set("view options", {layout: false});
-app.use('/', express.static(__dirname));
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+
+
 app.use(express.json());
 
 
 var textdata = "null";
 
-app.post("pages/cool", (req, res) => {
+app.post("/cool", (req, res) => {
   let text = req.body["text"];
   textdata = text;
   console.log("POST");
   res.end();
 }); 
 
-app.get("pages/cool", (req, res) => {
+app.get("/cool", (req, res) => {
   res.render("pages/cool", {data: textdata});
 });
 
