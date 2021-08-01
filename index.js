5e0dca34fae3feff0f714e704003fd3560a9e599
gam3boy1 <aadianand1234@gmail.com>
2021-08-01 16:24:53 +0800

    updated cool page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cool = require("cool-ascii-faces");
 const express = require('express');
+const axios = require("axios").default;
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
@@ -9,4 +10,21 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get("/cool", (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+axios.post("/cool", {
+  coolface: "wewe"
+})
+.then((result) => { console.log(result); })
+.catch((err) => { console.log(err); });
+
+// axios({
+//   method: "post",
+//   // baseURL: "https://limitless-hamlet-40360.herokuapp.com",
+//   url: "/cool",
+//   data: {
+//     coolface: "weewrewrewrew"
+//   }
+// })
+// .then((result) => { console.log(result); })
+// .catch((err) => { console.log(err); });
